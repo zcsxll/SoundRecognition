4b91f74b0a8086e5ee965a2c72e05849bbb4438c
zhaochengshuai <zhaochengshuai@sogou-inc.com>
2021-04-11 10:59:04 +0800

    add dataset.py and pack_util.py

---
+++ dataset.py
@@ -0,0 +1,87 @@
+import os
+import sys
+import torch
+import numpy as np
+import librosa
+import soundfile as sf
+import random
+import pack_util
+
+class TrainDataset(torch.utils.data.Dataset):
+    def __init__(self, root_dir):
+        types = os.listdir(root_dir)
+        types = [t for t in types if not t.endswith('.list')]
+        types = sorted(types) #按字母顺序排序
+        self.types2id = {}
+        self.id2types = {}
+        for i, t in enumerate(types):
+            self.types2id[t] = i
+            self.id2types[i] = t
+        self.types2id['other'] = len(types)
+        self.id2types[len(types)] = 'other'
+        '''
+        self.types2id的内容如下，按字母顺序排序
+        {'Breath': 0, 'Brushing teeth': 1, 'Clapping': 2, 'Coughing': 3, 'Door knock': 4, 'Drinking, sipping': 5, 'Footsteps': 6, 'Keyboard typing': 7, 'Laughing': 8, 'Sneezing': 9, 'Snoring': 10, 'other': 11}
+        '''
+
+        self.root_dir = root_dir
+        with open(os.path.join(root_dir, 'train_set.list')) as fp:
+            self.waves = fp.read().splitlines()
+
+        n_other = int(len(self.waves) * 0.1)
+        self.waves += ['other/666'] * n_other #后面的666不会被使用，随便写的
+
+        noise_list = ['/local/data/wind-noise-record.lmdb']
+        _, self.noise = pack_util.get_total(noise_list, keep_struct=False)
+        self.sess_dict = pack_util.init_session(noise_list)
+
+        speech_list = ['/local/data/from_zx/AEC/train_data/clean/clean-1000_vad_processed.lmdb']
+        speech_list += ['/local/data/from_zx/AEC/train_data/clean/read_speech_500remain.lmdb']
+        _, self.speech = pack_util.get_total(speech_list, keep_struct=False)
+        self.sess_dict.update(pack_util.init_session(speech_list))
+
+        self.other = self.noise + self.speech
+    
+    def __getitem__(self, idx):
+        wave = self.waves[idx]
+        type_name = os.path.split(wave)[0]
+        type_id = self.types2id[type_name]
+        if type_name != 'other':
+            wave_path = os.path.join(self.root_dir, wave)
+            pcm, sr = sf.read(wave_path)
+            pcm_16k = librosa.resample(pcm, sr, 16000)
+
+            if random.uniform(0, 1) <= 0.5: #均匀分布，50%情况加噪声
+                idx_noise = random.randint(0, len(self.noise) - 1)
+                noise = pack_util.load_audio(self.noise[idx_noise], self.sess_dict, 5)
+                snr = np.random.choice([3, 5, 7, 9, 11, 15])
+                # sf.write(f'./pcm_16k{snr}.wav', pcm_16k, 16000)
+                pcm_16k, _, _ = pack_util.gen_mix(pcm_16k, noise, snr, energy_norm=False)
+                # sf.write(f'./mix{snr}.wav', pcm_16k, 16000)
+        else: #这里加入一些不相关数据，有说话声和纯噪声
+            idx_other = random.randint(0, len(self.other) - 1)
+            pcm_16k = pack_util.load_audio(self.other[idx_other], self.sess_dict, 5)
+            # sf.write('./other.wav', pcm_16k, 16000)
+
+        spec = librosa.stft(pcm_16k, n_fft=1024, hop_length=512, window='hann')
+        spec = spec[1:, :].T #不使用0频率
+        spec_mag = np.abs(spec) #频域能量，不使用相位信息
+        # print(spec_mag.shape, spec_mag[10, 0:10])
+        sf.write(f'./type{type_id}.wav', pcm_16k, 16000)
+        return spec_mag, type_id #spec_mag的shape是[431, 512]
+
+    def __len__(self):
+        return len(self.waves)
+
+if __name__ == '__main__':
+    dataset = TrainDataset('/local/data/zcs/sound_set')
+    dataloader = torch.utils.data.DataLoader(
+        dataset=dataset,
+        batch_size=1,
+        shuffle=True,
+        num_workers=0)
+    print(len(dataset))
+    for idx, (feature, t) in enumerate(dataloader):
+        print(idx, feature.shape, t)
+        if idx >= 50:
+            break
---
+++ pack_util.py
@@ -0,0 +1,150 @@
+import os
+import io
+import numpy as np
+import lmdb
+import soundfile
+import librosa
+import random
+import zlib
+
+_ZLIB_H = [bytes.fromhex(h) for h in ['7801', '785E', '789C', '78DA']]
+
+def get_total(source_list, keep_struct=False):
+    total = 0
+    all_samples = []
+
+    for source in source_list:
+        if not os.path.exists(os.path.expanduser(source)):
+            raise ValueError('data source file is not exists: {}'.format(source))
+
+        extname = os.path.splitext(source)[-1].strip('.').split('.')[-1]
+        if extname in ['lmdb']:
+            env = lmdb.open(os.path.expanduser(source),
+                            readonly=True,
+                            lock=False,
+                            readahead=False,
+                            meminit=False,
+                            map_size=int(2**40))
+            n = env.stat()['entries']
+            total += n
+            samples = [['lmdb', source, i] for i in range(n)]
+            all_samples += [samples] if keep_struct else samples
+            env.close()
+        elif extname in ['flac', 'wav', 'pcm']:
+            total += 1
+            samples = [['file', os.path.expanduser(source)]]
+            all_samples += [samples] if keep_struct else samples
+        else:
+            raise NotImplementedError('not implemented: {}'.format(source))
+
+    return total, all_samples
+
+def init_session(source_list):
+    sess_dict = {}
+
+    for source in source_list:
+        if not os.path.exists(os.path.expanduser(source)):
+            raise ValueError('data source file is not exists: {}'.format(source))
+
+        extname = os.path.splitext(source)[-1].strip('.').split('.')[-1]
+        if extname in ['lmdb']:
+            env = lmdb.open(os.path.expanduser(source),
+                            readonly=True,
+                            lock=False,
+                            readahead=True,
+                            map_size=int(2**30))
+            txn = env.begin(write=False)
+            sess_dict.update({source:txn})
+        # else:
+        #     raise NotImplementedError('not implemented: {}'.format(source))
+    return sess_dict
+
+def load_from_soundfile(sf, seconds=-1):
+    frames = seconds * sf.samplerate if seconds > 0 else -1
+    if frames >= sf.frames or frames == -1:
+        data = sf.read(frames)
+    else:
+        offset = random.randint(0, sf.frames-frames-1)
+        # print(offset, frames, sf.frames)
+        try:
+            sf.seek(offset)
+            data = sf.read(frames)
+        except Exception as e:
+            print('Ignore exception in load_from_soundfile():', sf, e)
+            data = np.zeros(frames, dtype=np.float32)
+    return data, sf.samplerate
+
+def load_from_lmdb(sess, idx, seconds=-1):
+    try:
+        data = sess.get('{}'.format(idx).encode())
+        if data[:2] in _ZLIB_H:
+            data = zlib.decompress(data)
+        buff = io.BytesIO(data)
+        with soundfile.SoundFile(buff) as sf:
+            data, sr = load_from_soundfile(sf, seconds)
+        return data, sr
+    except Exception as e:
+        raise e
+
+def load_audio(sample, sess_dict, seconds=-1, pad=True, samplerate=16000):
+    '''
+    sample: list of length 3, the content is ['lmdb', 'lmdb_file_name', index]
+    sess_dict: dict of lmdb env, eg: {'lmdb_file_name1':env1, 'lmdb_file_name2':env2, ...}; we read FLAC from lmdb env
+    secondes: time to read
+    pad: 如果FLAC长度不足senconds，补零
+    samplerate: 如果FLAC解码后不是目标采样率，则重采样
+    '''
+    if sample[0] == 'lmdb':
+        _, sess_key, idx = sample
+        sess = sess_dict[sess_key]
+        data, sr = load_from_lmdb(sess, idx, seconds)
+    elif sample[0] == 'file':
+        filename = sample[1]
+        extname = os.path.splitext(filename)[-1]
+        if extname.lower() in ['.wav', '.flac']:
+            f = soundfile.SoundFile(filename)
+        elif extname.lower() in ['.pcm', '.raw']:
+            f = soundfile.SoundFile(filename,
+                            format='RAW',
+                            channels=1,
+                            samplerate=samplerate,
+                            subtype='PCM_16')
+        else:
+            raise ValueError(f'{sample} is not supported!')
+        data, sr = load_from_soundfile(f, seconds)
+    else:
+        raise NotImplementedError('not implemented: {}'.format(sample[0]))
+
+    if sr != samplerate:
+        data = librosa.resample(data, sr, samplerate)
+
+    if seconds != -1:
+        cur_len = data.shape[0]
+        if cur_len < seconds * samplerate:
+            data = np.pad(data, [0, seconds*samplerate-cur_len], mode='constant')
+    return data
+
+def gen_mix(clean, noise, snr, energy_norm=True):
+    if (np.sum(noise**2) * (10.0**(snr / 10.0))) == 0:
+        alpha = 1
+    else:
+        alpha = np.sqrt(np.sum(clean**2) / (np.sum(noise**2) * (10.0**(snr / 10.0))))
+
+    alpha = np.where(np.isnan(alpha), np.zeros_like(alpha), alpha)
+    noise = noise * alpha
+    mic = clean + noise
+
+    if energy_norm:
+        CONSTANT = 0.95  #energy nomalization
+        c = np.sqrt(CONSTANT * mic.size / np.sum(mic**2))
+    else:
+        peak = np.max(np.abs(mic))
+        if peak > 4:
+            mic = np.clip(mic, -4, 4)
+            c = 0.25
+        elif peak > 1:
+            c = 1 / peak
+        else:
+            c = 1
+
+    return mic * c, clean * c, noise * c
